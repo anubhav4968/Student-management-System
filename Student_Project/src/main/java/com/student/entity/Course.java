4a1907be87fbab1449257faf734d86757eb4fc9a
Anubhav Mishra <omanubhavmishra@gmail.com>
2022-12-21 13:55:05 +0530

    Some Update

--- Student_Project/src/main/java/com/student/entity/Course.java
+++ Student_Project/src/main/java/com/student/entity/Course.java
@@ -3,10 +3,10 @@ package com.student.entity;
 
 
 
-import jakarta.persistence.CascadeType;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -43,6 +43,6 @@ public class Course {
 	@Column(name ="Course_Topics", nullable = false, length=50)
 	private String courseTopics;
 	
-	@ManyToMany(mappedBy = "course", cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@ManyToMany(mappedBy = "course")
 	private Student student ;
 }
